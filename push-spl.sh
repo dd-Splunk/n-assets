8217fa627273ff2005dbac3adf9341a21f2fc238
D. Dessy <ddessy@splunk.com>
2022-04-12 09:05:39 +0200

    latest push

--- push-spl.sh
+++ push-spl.sh
@@ -10,9 +10,9 @@ echo "Uploading ${APP_FILE} ..."
 docker cp $APP_FILE so1:$APP_LOCATION
 echo "Asking Splunk to remove ${APP} ..."
 curl -X DELETE -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/apps/local/$APP
-# echo "Asking Splunk to install ${APP_FILE} ..."
-# curl -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/apps/local \
-#     -H 'Content-Type: application/x-www-form-urlencoded' \
-#     -d "name=${APP_LOCATION}&update=True&filename=True"
-# echo "Done."
-# #
+echo "Asking Splunk to install ${APP_FILE} ..."
+curl -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/apps/local \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -d "name=${APP_LOCATION}&update=True&filename=True"
+echo "Done."
+#
