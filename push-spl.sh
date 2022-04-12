5eaccd0b743dc9166e314399f8c50e99083ac909
D. Dessy <ddessy@splunk.com>
2022-04-12 15:28:36 +0200

    Eventgen samples

--- push-spl.sh
+++ push-spl.sh
@@ -13,5 +13,7 @@ echo "Asking Splunk to install ${APP_FILE} ..."
 curl -s -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/apps/local \
     -H 'Content-Type: application/x-www-form-urlencoded' \
     -d "name=${APP_LOCATION}&update=True&filename=True"
+echo "Restaritng"
+curl -s -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/server/control/restart 
 echo "Done."
 #
