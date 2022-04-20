3edb25dadc8468605aeb3d9f7825ccbaca635b90
D. Dessy <ddessy@splunk.com>
2022-04-20 14:42:11 +0200

    Adding initialization script

--- init-data.sh
+++ init-data.sh
@@ -12,13 +12,6 @@ do
     echo "Splunk oneShot $f"
     curl  -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/data/inputs/oneshot \
         -d "name=/tmp/$f"
-done
-
-# echo "Asking Splunk to install ${APP_FILE} ..."
-# curl -s -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/apps/local \
-#     -H 'Content-Type: application/x-www-form-urlencoded' \
-#     -d "name=${APP_LOCATION}&update=True&filename=True"
-# echo "Restaritng"
-# curl -s -X POST -k -u admin:$SPLUNK_PASSWORD https://$SPLUNK_HOST:8089/services/server/control/restart 
-# echo "Done."
+done 
+echo "Done."
 #
